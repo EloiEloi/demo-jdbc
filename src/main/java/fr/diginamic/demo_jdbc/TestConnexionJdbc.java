0e91789a1b62063cd5f917fd415721612743440c
Eloi Turpin <eloi.turpin@gmail.com>
2019-05-20 15:22:47 +0200

    ajout des try catch de la connexion à la BDD

--- src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
@@ -8,6 +8,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import fr.diginamic.exceptions.TechnicalException;
+
 /**
  * @author Eloi
  *
@@ -19,20 +21,39 @@ public class TestConnexionJdbc {
 	 * @throws SQLException
 	 * @throws ClassNotFoundException
 	 */
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
-
 		String driverName = monFichierConf.getString("database.driver");
-
 		String Url = monFichierConf.getString("database.url");
 		String User = monFichierConf.getString("database.user");
 		String Password = monFichierConf.getString("database.password");
 
-		Class.forName(driverName);
+		try {
 
-		Connection maConnection = DriverManager.getConnection(Url, User, Password);
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			throw new TechnicalException("Le driver JBDC " + driverName + " n'a pas été trouvé.", e);
+		}
+
+		Connection maConnection = null;
+		try {
+			maConnection = DriverManager.getConnection(Url, User, Password);
+		} catch (SQLException e) {
+			throw new TechnicalException("Impossible de se connecter à la BDD", e);
+
+		} finally {
+			try {
+				if (maConnection != null) {
+					maConnection.close();
+				}
+
+			} catch (SQLException e) {
+				throw new TechnicalException("iImpossible de fermer la connection à la BDD", e);
+			}
+
+		}
 
 		System.out.println(maConnection);
 
